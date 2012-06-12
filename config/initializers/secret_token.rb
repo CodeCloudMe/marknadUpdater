8109ea94b526e3e211581f2e1fb1b871f7b4af23
Fotios Lindiakos <fotios@redhat.com>
2012-06-12 17:22:08 -0400

    Moved secret_generation function to their own lib file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,5 @@
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
+
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
+
 # Be sure to restart your server when you modify this file.
 
 # Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
