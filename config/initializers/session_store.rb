393ba2c4c89db26a1008f6e22b6e32b239e01e25
Fotios Lindiakos <fotios@redhat.com>
2012-07-17 22:49:32 -0400

    Added OpenShift security functions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,14 @@
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
+
 # Be sure to restart your server when you modify this file.
 
-RailsApp::Application.config.session_store :cookie_store, key: '_rails_app_session'
+# Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
+RailsApp::Application.config.session_store :cookie_store, :key => initialize_secret(
+  :session_store,
+  '_railsapp_session'
+)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# RailsApp::Application.config.session_store :active_record_store
+# Railsapp::Application.config.session_store :active_record_store
